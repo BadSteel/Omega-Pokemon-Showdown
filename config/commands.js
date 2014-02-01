2a451cdd0582e49fb42fd81a3ba2278b3274c51d
小太 <nospam@xiaotai.org>
2014-02-01 22:58:00 +1100

    Add /hideauth

--- config/commands.js
+++ config/commands.js
@@ -1068,6 +1068,34 @@ var commands = exports.commands = {
 		}
 	},
 
+	hide: 'hideauth',
+	hideauth: function(target, room, user) {
+		if (!this.can('hideauth')) return false;
+		target = target || config.groups.default.global;
+		if (!config.groups[target]) {
+			target = config.groupsranking[0];
+			this.sendReply("You have picked an invalid group, defaulting to '" + target + "'.");
+		} else if (config.groups[target].rank >= config.groups[user.group].rank)
+			return this.sendReply("The group you have chosen is either your current group OR one of higher rank. You cannot hide like that.");
+
+		user.getIdentity = function (roomid) {
+			var identity = Object.getPrototypeOf(this).getIdentity.call(this, roomid);
+			if (identity[0] === this.group)
+				return target + identity.slice(1);
+			return identity;
+		};
+		user.updateIdentity();
+		return this.sendReply("You are now hiding your auth as '" + target + "'.");
+	},
+
+	show: 'showauth',
+	showauth: function(target, room, user) {
+		if (!this.can('hideauth')) return false;
+		delete user.getIdentity;
+		user.updateIdentity();
+		return this.sendReply("You are now showing your authority!");
+	},
+
 	/*********************************************************
 	 * Help commands
 	 *********************************************************/
